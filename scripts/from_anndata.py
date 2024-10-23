b910545323a1c3cd0c7199de28ee964dcf02db49
namemcguffin@epiphyte <52169277+namemcguffin@users.noreply.github.com>
2024-10-23 15:34:09 -0400

    dev: refactored `from_anndata.py` to make functionality importable.

--- scripts/from_anndata.py
+++ scripts/from_anndata.py
@@ -1,20 +1,19 @@
 import sys
 import os
-from anndata import read_h5ad
+from pathlib import Path
+from anndata import read_h5ad, AnnData
 from pandas import DataFrame
 
-if __name__ == "__main__":
-    adata = read_h5ad(sys.argv[1])
-    out_path = sys.argv[2]
-    spatial_data = adata.obsm[sys.argv[3] if len(sys.argv) >= 4 else "spatial"]
-    mtx = adata.layers[sys.argv[4]] if len(sys.argv) >= 5 else adata.X
+def from_anndata(adata: AnnData, out_path: Path, spatial_slot: str = "spatial", layer: str | None = None):
+    spatial_data = adata.obsm[spatial_slot]
+    mtx = adata.layers[layer] if layer is not None else adata.X
 
     os.makedirs(out_path, exist_ok=True)
-    os.makedirs(os.path.join(out_path, "feat"), exist_ok=True)
+    os.makedirs(out_path / "feat", exist_ok=True)
 
     DataFrame(
         {"cell": adata.obs_names, "x": spatial_data[:, 0], "y": spatial_data[:, 1]}
-    ).to_csv(os.path.join(out_path, "cells.tsv"), sep="\t", index=False)
+    ).to_csv(out_path / "cells.tsv", sep="\t", index=False)
 
     for i, feat in enumerate(adata.var_names):
         nz = mtx[:, i].nonzero()[0]
@@ -24,3 +23,11 @@ if __name__ == "__main__":
                 "expr": (mtx[j, i] for j in nz),
             }
         ).to_csv(os.path.join(out_path, "feat", f"{feat}.tsv"), sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    from_anndata(
+        read_h5ad(sys.argv[1]),
+        Path(sys.argv[2]),
+        *(sys.argv[3:5])
+    )
